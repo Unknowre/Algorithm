2227a818a631ca3eabe4fe881225b3176e6e1252
UNKNOW <58127457+Unknowre@users.noreply.github.com>
2025-09-05 11:38:06 +0700

    Add files via upload

---
+++ n.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class n {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("ป้อนจำนวนเต็ม: ");
+        int number = scanner.nextInt();
+        if (number % 2 == 0) {
+            System.out.println("เลขคู่");
+        } else {
+            System.out.println("เลขคี่");
+        }
+        System.out.println("จบ");
+        scanner.close();
+    }
+}
